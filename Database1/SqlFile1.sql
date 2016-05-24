c8665463a972b608461805757d055e85e614e44f
Dmitry Kovalskiy <dmitry.kovalskiy@me.com>
2016-05-24 16:25:59 +0300

    first checkin

---
+++ Database1/SqlFile1.sql
@@ -0,0 +1,16 @@
+﻿;WITH FirstSales(productId,customerId, [dateTime])as
+(
+	SELECT ProductId,customerId, MIN[dateTime]
+	FROM Sales(NOLOCK)
+	GROUP BY ProductId,customerId
+),
+SalesId(saleId) AS
+(
+	SELECT saleId FROM Sales (NOLOCK) as s
+	INNER JOIN FirstSales (NOLOCK) as fs ON s.productId = fs.productId 
+								AND s.customerId = fs.customerId
+								AND s.dateTime = fs.dateTime
+)
+SELECT productId,COUNT(1) FROM Sales as s(NOLOCK)
+INNER JOIN SalesId as sid (NOLOCK) ON s.saleId = sid.saleId
+GROUP BY productId
